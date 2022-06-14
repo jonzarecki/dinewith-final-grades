2da750dbc11b9cc3b14b6f4c060c36b01f505067
Jonathan Zarecki <yonilx@gmail.com>
2022-06-14 14:29:55 +0300

    fix: remove docs build from nox
    .

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,14 @@
 """Nox sessions."""
 import os
-import shutil
 from pathlib import Path
 
 import nox
 import toml
-from nox import Session
 
 package = "dinewith"
 python_versions = ["3.9"]
 nox.needs_version = ">= 2021.6.6"
-nox.options.sessions = ("docs-build",)  # "tests", "xdoctest",
+nox.options.sessions = []  # "tests"
 pyproject_data = toml.loads(Path("pyproject.toml").read_text())
 submodule_paths = []
 if os.path.exists(".gitmodules"):
@@ -50,37 +48,3 @@ if os.path.exists(".gitmodules"):
 #         # keep .coverage.* files if not interactive (i.e. CI)
 #         sess.run(*(["coverage", "combine"] + (["--keep"] if not sess.interactive else [])))
 #     sess.run("coverage", *args)
-
-
-@nox.session(python=False)
-def xdoctest(sess: Session) -> None:
-    """Run examples with xdoctest."""
-    args = sess.posargs or ["all"]
-    sess.install("xdoctest[colors]")
-    sess.run("python", "-m", "xdoctest", package, *args)
-
-
-@nox.session(name="docs-build", python=python_versions)
-def docs_build(sess: Session) -> None:
-    """Build the documentation."""
-    args = sess.posargs or ["docs/source", "docs/_build"]
-    sess.install("-r", "docs/source/requirements.txt")
-
-    build_dir = Path("docs", "_build")
-    if build_dir.exists():
-        shutil.rmtree(build_dir)
-
-    sess.run("sphinx-build", *args)
-
-
-@nox.session(python=python_versions)
-def docs(sess: Session) -> None:
-    """Build and serve the documentation with live reloading on file changes."""
-    args = sess.posargs or ["--open-browser", "docs/source", "docs/_build"]
-    sess.install("-r", "docs/source/requirements.txt")
-
-    build_dir = Path("docs", "_build")
-    if build_dir.exists():
-        shutil.rmtree(build_dir)
-
-    sess.run("sphinx-autobuild", *args)
