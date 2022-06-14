e0a00f4fea7c46aadecfc0586826bdc7e6e13d3c
Jonathan Zarecki <yonilx@gmail.com>
2022-06-14 14:12:50 +0300

    fix: change references to 3.9
    .

--- noxfile.py
+++ noxfile.py
@@ -9,9 +9,9 @@ import toml
 from nox import Session
 
 package = "dinewith"
-python_versions = ["3.8"]
+python_versions = ["3.9"]
 nox.needs_version = ">= 2021.6.6"
-nox.options.sessions = ("tests", "xdoctest", "docs-build")
+nox.options.sessions = ("docs-build")  # "tests", "xdoctest",
 pyproject_data = toml.loads(Path("pyproject.toml").read_text())
 submodule_paths = []
 if os.path.exists(".gitmodules"):
@@ -21,36 +21,36 @@ if os.path.exists(".gitmodules"):
         submodule_paths.append("common")
 
 
-@nox.session(python=False)
-def tests(sess: Session) -> None:
-    """Run the test suite."""
-    sess.install("coverage[toml]", "pytest", "pygments")
-
-    def add_quotes_and_join(lst: List[str]) -> str:
-        return ",".join([f"{s}" for s in lst])
-
-    omit_paths = ["--omit"] + [
-        add_quotes_and_join(pyproject_data["tool"]["coverage"]["run"]["omit"] + [f"{p}/**" for p in submodule_paths])
-    ]
-    run_paths = [p for p in pyproject_data["tool"]["coverage"]["run"]["source"] if p not in submodule_paths]
-
-    try:
-        sess.run("coverage", "run", "--parallel", *omit_paths, "-m", "pytest", *run_paths, *sess.posargs)
-    finally:
-        sess.notify("coverage", posargs=[])
-
-
-@nox.session(python=python_versions)
-def coverage(sess: Session) -> None:
-    """Produce the coverage report."""
-    args = sess.posargs or ["report"]
-
-    sess.install("coverage[toml]")
-
-    if not sess.posargs and any(Path().glob(".cache/.coverage.*")):
-        # keep .coverage.* files if not interactive (i.e. CI)
-        sess.run(*(["coverage", "combine"] + (["--keep"] if not sess.interactive else [])))
-    sess.run("coverage", *args)
+# @nox.session(python=False)
+# def tests(sess: Session) -> None:
+#     """Run the test suite."""
+#     sess.install("coverage[toml]", "pytest", "pygments")
+#
+#     def add_quotes_and_join(lst: List[str]) -> str:
+#         return ",".join([f"{s}" for s in lst])
+#
+#     omit_paths = ["--omit"] + [
+#         add_quotes_and_join(pyproject_data["tool"]["coverage"]["run"]["omit"] + [f"{p}/**" for p in submodule_paths])
+#     ]
+#     run_paths = [p for p in pyproject_data["tool"]["coverage"]["run"]["source"] if p not in submodule_paths]
+#
+#     try:
+#         sess.run("coverage", "run", "--parallel", *omit_paths, "-m", "pytest", *run_paths, *sess.posargs)
+#     finally:
+#         sess.notify("coverage", posargs=[])
+#
+#
+# @nox.session(python=python_versions)
+# def coverage(sess: Session) -> None:
+#     """Produce the coverage report."""
+#     args = sess.posargs or ["report"]
+#
+#     sess.install("coverage[toml]")
+#
+#     if not sess.posargs and any(Path().glob(".cache/.coverage.*")):
+#         # keep .coverage.* files if not interactive (i.e. CI)
+#         sess.run(*(["coverage", "combine"] + (["--keep"] if not sess.interactive else [])))
+#     sess.run("coverage", *args)
 
 
 @nox.session(python=False)
